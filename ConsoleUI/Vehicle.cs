c1af9707f4b06780e024713b7683c5d9f19b81b5
Kylie Paykoff <kyliepaykoff@gmail.com>
2024-10-14 15:26:15 -0400

    first commit

---
+++ ConsoleUI/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    internal abstract class Vehicle
+    {
+        public string Year { get; set; } = "Default value";
+        public string Make { get; set; } = "Default value";
+        public string Model { get; set; } = "Default value";
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+
+            Console.WriteLine($"Default virtual drive value");
+
+        
+        }
+        
+
+
+
+    }
+}
